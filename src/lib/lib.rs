2a0c6aeba355f6199fbf424da11b7f4585b778f6
Ex-32 <jenna@fligor.net>
2023-08-30 22:53:59 -0500

    dependency bump

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,11 +1,8 @@
 /// A library for parsing math expressions with stateful variables
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
 mod ast;
 pub mod context;
 pub mod error;
 mod parser;
 
 pub use rug::Integer;
+
--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -1,3 +1,4 @@
+use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "lib/grammar/math.pest"]
 pub(crate) struct MathParser;
